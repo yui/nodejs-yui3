b2b719ccf5fb8388b1de9717b6a314de7ca311c2
Dav Glass <davglass@gmail.com>
2010-03-22 23:49:11 -0700

    Semi working io-node module. Needs 2 patches applied to Y.io to work

---
+++ test-io.js
@@ -0,0 +1,41 @@
+var sys = require('sys');
+
+var YUI = require("./lib/node-yui3").YUI;
+
+// TODO: This should pass, but currently doesn't.
+// This will work for YUI core, but any submodules are in different files
+// This will work better once the 3.1.0 version of YUI is available
+// And you can combo handle all the submodules into one request.
+//require("assert").equal( global.YUI, undefined, "global yui created");
+
+
+//Now use non-DOM related YUI utilities
+YUI({
+    //Only set these if you want to load locally
+    loaderPath: 'loader/loader-debug.js',
+    base: './yui3/build/',
+    filter: 'debug',
+    debug: true
+}).use('json', 'base', 'io-node', function(Y) {
+
+    var url = 'http:/'+'/yuilibrary.com/gallery/api/user/davglass';
+    
+    //url = 'http:/'+'/query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20location%3D90210&format=json';
+    
+    Y.io(url, {
+        xdr: {
+            use: 'node'
+        },
+        on: {
+            start: function() {
+                Y.log('Start IO', 'info', 'TEST');
+            },
+            success: function(id, o) {
+                //Y.log(o.responseText);
+                Y.log(Y.JSON.parse(o.responseText));
+            }
+        }
+    });
+    
+
+});
