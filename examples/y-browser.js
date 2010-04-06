c4facb3519b1b583cd6ed78c82e4de66fe246e19
Dav Glass <davglass@gmail.com>
2010-04-06 15:11:16 -0700

    Added Y Browser example

---
+++ examples/y-browser.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+var sys = require('sys'),
+    YUI = require("../lib/node-yui3").YUI;
+
+//Now use non-DOM related YUI utilities
+YUI({
+    //Only set these if you want to load locally
+    loaderPath: 'loader/loader-debug.js',
+    base: './yui3/build/',
+    filter: 'debug',
+    _logExclude: {
+        'attribute': true,
+        'base': true,
+        'get': true,
+        'loader': true,
+        'yui': true,
+        'widget': true,
+        'event': true
+    },
+    debug: true
+}).use('nodejs-dom', 'node', function(Y) {
+    
+    var document = Y.Browser.document;
+    var window = Y.Browser.window;
+    var self = Y.Browser.self;
+    var navigator = Y.Browser.navigator;
+    var location = Y.Browser.location;
+
+    //With the local aliases
+    var el = document.createElement('div');
+    el.id = 'foo';
+    el.innerHTML = '<em>This is a test</em> This <strong class="odd">is another</strong> test ';
+    document.body.appendChild(el);
+
+
+    //SCOPED
+    var el2 = Y.Browser.document.createElement('div');
+    el2.id = 'foo2bar';
+    el2.innerHTML = '<em class="odd">This is a test</em> This <strong>is another</strong> test ';
+    Y.Browser.document.body.appendChild(el2);
+
+    sys.puts('getElementByid(foo2bar): ' + Y.Browser.document.getElementById('foo2bar'));
+    sys.puts('getElementByid(foo): ' + Y.Browser.document.getElementById('foo'));
+    sys.puts('getElementByTagName(em): ' + Y.Browser.document.getElementsByTagName('em'));
+    sys.puts('getElementByClassName(odd): ' + Y.Browser.document.getElementsByClassName('odd'));
+
+    sys.puts('');
+    sys.puts('Y.Browser.document.body.outerHTML: ');
+    sys.puts(Y.Browser.document.body.outerHTML);
+
+
+
+
+
+    Y.log(Y.Browser.document.body.outerHTML);
+});
