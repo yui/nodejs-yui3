637022b14abb20c3ee1e33a346eba8c531ef5776
Dav Glass <davglass@gmail.com>
2010-05-03 09:27:54 -0700

    Added test page for comments in HTML templates

---
+++ tests/comments.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+var sys = require('sys'),
+    fs = require('fs');
+
+var YUI = require("../lib/node-yui3").YUI;
+
+require("assert").equal( global.YUI, undefined, "global yui created");
+
+
+YUI({
+    filter: 'debug',
+    logExclude: {
+        'attribute': true,
+        'base': true,
+        'get': true,
+        'loader': true,
+        'yui': true,
+        'widget': true,
+        'event': true
+    },
+    debug: true
+}).use('nodejs-dom', function(Y) {
+
+    Y.log('JSDom testing..');
+
+    fs.readFile('./comments.html', encoding="utf-8", function(err, data) {
+
+        Y.config.doc.body.innerHTML = data;
+        
+        Y.log('Document loaded');
+        Y.log(Y.config.doc.outerHTML);
+        
+    });
+
+    
+
+
+
+
+});
