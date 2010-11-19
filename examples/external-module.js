603fa8f79d54e46af555d82e9a4573b0424bb53a
Dav Glass <davglass@gmail.com>
2010-11-18 20:20:43 -0600

    Code cleanup, more readable now

--- examples/external-module.js
+++ examples/external-module.js
@@ -1,5 +1,22 @@
 YUI.add('external-foo', function(Y) {
 
+    Y.log(__dirname, 'info', '__dirname');
+    Y.log(__filename, 'info', '__filename');
+
+    //console.log(require);
+    //console.log(process);
+
+    var sys = require('sys');
+    sys.puts('PRINTED FROM INTERNAL REQUIRE WITH sys.puts');
+
+    var config = require('./config');
+    Y.log(config);
+    config.foo('#1 Relative');
+
+    var config = require(__dirname + '/config');
+    Y.log(config);
+    config.foo('#2 Full Dir Require');
+
     Y.log('EXTERNAL FOO LOADED');
 
 }, '1.0.0', { requires: ['node'] });
