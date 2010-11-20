764fec8ba75fa8685e2e0842dac7fe1efb140a2e
Dav Glass <davglass@gmail.com>
2010-11-19 19:28:30 -0600

    Added parallel example

---
+++ examples/parallel.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var YUI = require("yui3").YUI;
+require("assert").equal( global.YUI, undefined, "global yui created");
+
+
+YUI({
+    filter: 'debug',
+    debug: true
+}).use('parallel', function(Y) {
+    
+    Y.log('Reading this directory and reading the contents of each file..');
+    var stack = new Y.Parallel();
+    fs.readdir(__dirname, stack.add(function(err, files) {
+        files.forEach(function(f) {
+            fs.readFile(__dirname + f, stack.add(function(err, data) {
+                //Do something here..
+            }));
+        });
+    }));
+    stack.done(function() {
+        Y.log('All callbacks have fired..');
+    });
+
+});
